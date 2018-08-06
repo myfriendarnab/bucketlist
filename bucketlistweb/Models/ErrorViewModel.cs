bf6498dd506a4a7703cc07fdcf9c04f022d48990
arnab bhattacharya <myfriendarnab@gmail.com>
2018-08-06 07:06:36 +0530

    my first commit

---
+++ bucketlistweb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bucketlistweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
